12f3413613395145124acffd175c091953a3969e
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-07-28 13:32:22 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by 山岡巧 on 2020/07/28.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
